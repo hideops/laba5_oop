0e2588e428dd51aaacbd5c65a66a6a3f97ef9675
hideops <128249753+hideops@users.noreply.github.com>
2025-11-21 10:25:52 -0800

    Update pmr_queue.h

--- pmr_queue.h
+++ pmr_queue.h
@@ -1,79 +1,95 @@
 #pragma once
+
 #include <memory_resource>
 #include <list>
 #include <iterator>
 #include <iostream>
 #include <cstddef>
+#include <algorithm>
+#include <utility>
 
-class DynamicMemoryResource : public std::pmr::memory_resource {
-private:
-    struct BlockInfo {
-        void* ptr;
-        std::size_t size;
-        BlockInfo(void* p, std::size_t s) : ptr(p), size(s) {}
+class DynamicMemoryResource final : public std::pmr::memory_resource 
+{
+    struct Block 
+    {
+        void* address;
+        std::size_t bytes;
+        Block(void* addr, std::size_t size) : address(addr), bytes(size) {}
     };
     
-    std::list<BlockInfo> allocated_blocks_;
-    std::pmr::memory_resource* upstream_;
+    std::list<Block> active_blocks;
+    std::pmr::memory_resource* parent_resource;
 
 public:
-    explicit DynamicMemoryResource(std::pmr::memory_resource* upstream = 
-                                  std::pmr::get_default_resource()) 
-        : upstream_(upstream) {}
+    explicit DynamicMemoryResource(std::pmr::memory_resource* parent = std::pmr::get_default_resource()) 
+        : parent_resource(parent) 
+    {
+    }
     
     DynamicMemoryResource(const DynamicMemoryResource&) = delete;
     DynamicMemoryResource& operator=(const DynamicMemoryResource&) = delete;
     
-    ~DynamicMemoryResource() override {
-        for (const auto& block : allocated_blocks_) {
-            upstream_->deallocate(block.ptr, block.size);
+    ~DynamicMemoryResource() 
+    {
+        for (const Block& block : active_blocks) 
+        {
+            parent_resource->deallocate(block.address, block.bytes);
         }
     }
 
-protected:
-    void* do_allocate(std::size_t bytes, std::size_t alignment) override {
-        void* ptr = upstream_->allocate(bytes, alignment);
-        try {
-            allocated_blocks_.emplace_back(ptr, bytes);
-        } catch (...) {
-            upstream_->deallocate(ptr, bytes, alignment);
+private:
+    void* do_allocate(std::size_t bytes, std::size_t alignment) override 
+    {
+        void* new_block = parent_resource->allocate(bytes, alignment);
+        
+        try 
+        {
+            active_blocks.emplace_back(new_block, bytes);
+        } 
+        catch (...) 
+        {
+            parent_resource->deallocate(new_block, bytes, alignment);
             throw;
         }
-        return ptr;
+        
+        return new_block;
     }
     
-    void do_deallocate(void* ptr, std::size_t bytes, 
-                      std::size_t alignment) override {
-        auto it = std::find_if(allocated_blocks_.begin(), allocated_blocks_.end(),
-                              [ptr](const BlockInfo& block) {
-                                  return block.ptr == ptr;
-                              });
+    void do_deallocate(void* ptr, std::size_t bytes, std::size_t alignment) override 
+    {
+        auto found = std::find_if(active_blocks.begin(), active_blocks.end(),
+            [ptr](const Block& block) { return block.address == ptr; });
         
-        if (it != allocated_blocks_.end()) {
-            upstream_->deallocate(ptr, bytes, alignment);
-            allocated_blocks_.erase(it);
+        if (found != active_blocks.end()) 
+        {
+            parent_resource->deallocate(ptr, bytes, alignment);
+            active_blocks.erase(found);
         }
     }
     
-    bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override {
+    bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override 
+    {
         return this == &other;
     }
 };
 
 template<typename T>
-struct QueueNode {
-    T value;
-    QueueNode* next;
+struct QueueNode 
+{
+    T data;
+    QueueNode* next_node;
     
     template<typename... Args>
     QueueNode(Args&&... args) 
-        : value(std::forward<Args>(args)...), next(nullptr) {}
+        : data(std::forward<Args>(args)...), next_node(nullptr) 
+    {
+    }
 };
 
 template<typename T>
-class QueueIterator {
-private:
-    QueueNode<T>* current_;
+class QueueIterator 
+{
+    QueueNode<T>* current_node;
 
 public:
     using iterator_category = std::forward_iterator_tag;
@@ -82,182 +98,231 @@ public:
     using pointer = T*;
     using reference = T&;
 
-    explicit QueueIterator(QueueNode<T>* node = nullptr) : current_(node) {}
+    explicit QueueIterator(QueueNode<T>* node = nullptr) : current_node(node) {}
 
-    QueueIterator& operator++() {
-        if (current_) {
-            current_ = current_->next;
+    QueueIterator& operator++() 
+    {
+        if (current_node) 
+        {
+            current_node = current_node->next_node;
         }
         return *this;
     }
 
-    QueueIterator operator++(int) {
-        QueueIterator temp = *this;
+    QueueIterator operator++(int) 
+    {
+        QueueIterator old = *this;
         ++(*this);
-        return temp;
+        return old;
     }
 
-    reference operator*() const { 
-        return current_->value; 
+    reference operator*() const 
+    { 
+        return current_node->data; 
     }
     
-    pointer operator->() const { 
-        return &current_->value; 
+    pointer operator->() const 
+    { 
+        return &current_node->data; 
     }
 
-    bool operator==(const QueueIterator& other) const {
-        return current_ == other.current_;
+    bool operator==(const QueueIterator& other) const 
+    {
+        return current_node == other.current_node;
     }
     
-    bool operator!=(const QueueIterator& other) const {
-        return !(*this == other);
+    bool operator!=(const QueueIterator& other) const 
+    {
+        return current_node != other.current_node;
     }
 };
 
 template<typename T>
-class PmrQueue {
-private:
-    using allocator_type = std::pmr::polymorphic_allocator<QueueNode<T>>;
+class PmrQueue 
+{
+    using NodeAllocator = std::pmr::polymorphic_allocator<QueueNode<T>>;
     
-    QueueNode<T>* head_;
-    QueueNode<T>* tail_;
-    allocator_type allocator_;
-    std::size_t size_;
+    QueueNode<T>* head_node;
+    QueueNode<T>* tail_node;
+    NodeAllocator node_allocator;
+    std::size_t element_count;
 
 public:
     using iterator = QueueIterator<T>;
     using const_iterator = QueueIterator<const T>;
 
-    explicit PmrQueue(std::pmr::memory_resource* mr = 
-                     std::pmr::get_default_resource()) 
-        : head_(nullptr), tail_(nullptr), allocator_(mr), size_(0) {}
+    explicit PmrQueue(std::pmr::memory_resource* resource = std::pmr::get_default_resource()) 
+        : head_node(nullptr), tail_node(nullptr), node_allocator(resource), element_count(0) 
+    {
+    }
     
-    ~PmrQueue() {
-        clear();
+    ~PmrQueue() 
+    {
+        clear_elements();
     }
     
     PmrQueue(const PmrQueue&) = delete;
     PmrQueue& operator=(const PmrQueue&) = delete;
     
     PmrQueue(PmrQueue&& other) noexcept 
-        : head_(other.head_), tail_(other.tail_), 
-          allocator_(other.allocator_), size_(other.size_) {
-        other.head_ = nullptr;
-        other.tail_ = nullptr;
-        other.size_ = 0;
+        : head_node(other.head_node), 
+          tail_node(other.tail_node), 
+          node_allocator(other.node_allocator), 
+          element_count(other.element_count) 
+    {
+        other.head_node = nullptr;
+        other.tail_node = nullptr;
+        other.element_count = 0;
     }
     
-    PmrQueue& operator=(PmrQueue&& other) noexcept {
-        if (this != &other) {
-            clear();
-            head_ = other.head_;
-            tail_ = other.tail_;
-            allocator_ = other.allocator_;
-            size_ = other.size_;
+    PmrQueue& operator=(PmrQueue&& other) noexcept 
+    {
+        if (this != &other) 
+        {
+            clear_elements();
             
-            other.head_ = nullptr;
-            other.tail_ = nullptr;
-            other.size_ = 0;
+            head_node = other.head_node;
+            tail_node = other.tail_node;
+            node_allocator = other.node_allocator;
+            element_count = other.element_count;
+            
+            other.head_node = nullptr;
+            other.tail_node = nullptr;
+            other.element_count = 0;
         }
         return *this;
     }
     
     template<typename U>
-    void push(U&& value) {
-        QueueNode<T>* new_node = allocator_.allocate(1);
-        try {
-            allocator_.construct(new_node, std::forward<U>(value));
-        } catch (...) {
-            allocator_.deallocate(new_node, 1);
+    void push(U&& element) 
+    {
+        QueueNode<T>* new_node = node_allocator.allocate(1);
+        try 
+        {
+            node_allocator.construct(new_node, std::forward<U>(element));
+        } 
+        catch (...) 
+        {
+            node_allocator.deallocate(new_node, 1);
             throw;
         }
         
-        if (tail_) {
-            tail_->next = new_node;
-        } else {
-            head_ = new_node;
+        if (tail_node) 
+        {
+            tail_node->next_node = new_node;
+        } 
+        else 
+        {
+            head_node = new_node;
         }
-        tail_ = new_node;
-        ++size_;
+        tail_node = new_node;
+        ++element_count;
     }
     
-    void pop() {
-        if (!head_) return;
+    void pop() 
+    {
+        if (!head_node) return;
         
-        QueueNode<T>* old_head = head_;
-        head_ = head_->next;
+        QueueNode<T>* old_head = head_node;
+        head_node = head_node->next_node;
         
-        if (!head_) {
-            tail_ = nullptr;
+        if (!head_node) 
+        {
+            tail_node = nullptr;
         }
         
-        allocator_.destroy(old_head);
-        allocator_.deallocate(old_head, 1);
-        --size_;
+        node_allocator.destroy(old_head);
+        node_allocator.deallocate(old_head, 1);
+        --element_count;
     }
     
-    T& front() { 
-        return head_->value; 
+    T& front() 
+    { 
+        return head_node->data; 
     }
     
-    const T& front() const { 
-        return head_->value; 
+    const T& front() const 
+    { 
+        return head_node->data; 
     }
     
-    bool empty() const { 
-        return head_ == nullptr; 
+    bool empty() const 
+    { 
+        return head_node == nullptr; 
     }
     
-    std::size_t size() const { 
-        return size_; 
+    std::size_t size() const 
+    { 
+        return element_count; 
     }
     
-    void clear() {
-        while (!empty()) {
-            pop();
-        }
+    void clear() 
+    {
+        clear_elements();
     }
     
-    iterator begin() { 
-        return iterator(head_); 
+    iterator begin() 
+    { 
+        return iterator(head_node); 
     }
     
-    iterator end() { 
+    iterator end() 
+    { 
         return iterator(nullptr); 
     }
     
-    const_iterator begin() const { 
-        return const_iterator(head_); 
+    const_iterator begin() const 
+    { 
+        return const_iterator(head_node); 
     }
     
-    const_iterator end() const { 
+    const_iterator end() const 
+    { 
         return const_iterator(nullptr); 
     }
     
-    const_iterator cbegin() const { 
-        return const_iterator(head_); 
+    const_iterator cbegin() const 
+    { 
+        return const_iterator(head_node); 
     }
     
-    const_iterator cend() const { 
+    const_iterator cend() const 
+    { 
         return const_iterator(nullptr); 
+    }
+
+private:
+    void clear_elements() 
+    {
+        while (!empty()) 
+        {
+            pop();
+        }
     }
 };
 
-struct ComplexType {
-    int id;
-    double value;
-    std::string name;
+struct ComplexType 
+{
+    int identifier;
+    double data_value;
+    std::string text_name;
     
-    ComplexType(int i, double v, const std::string& n) 
-        : id(i), value(v), name(n) {}
-    
-    friend std::ostream& operator<<(std::ostream& os, const ComplexType& ct) {
-        os << "ComplexType{id=" << ct.id << ", value=" << ct.value 
-           << ", name=\"" << ct.name << "\"}";
-        return os;
+    ComplexType(int id, double value, const std::string& name) 
+        : identifier(id), data_value(value), text_name(name) 
+    {
     }
     
-    bool operator==(const ComplexType& other) const {
-        return id == other.id && value == other.value && name == other.name;
+    friend std::ostream& operator<<(std::ostream& output, const ComplexType& obj) 
+    {
+        output << "ComplexType{id=" << obj.identifier << ", value=" << obj.data_value 
+               << ", name=\"" << obj.text_name << "\"}";
+        return output;
+    }
+    
+    bool operator==(const ComplexType& other) const 
+    {
+        return identifier == other.identifier && 
+               data_value == other.data_value && 
+               text_name == other.text_name;
     }
 };
